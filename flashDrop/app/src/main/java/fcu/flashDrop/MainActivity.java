b31119bbeaa12d8968c0674ee6bc8feb8d8d146e
chameleon <1730171838@qq.com>
2022-05-15 18:25:21 +0800

    更新標題欄及跳轉

--- flashDrop/app/src/main/java/fcu/flashDrop/MainActivity.java
+++ flashDrop/app/src/main/java/fcu/flashDrop/MainActivity.java
@@ -2,12 +2,15 @@ package fcu.flashDrop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -19,28 +22,35 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private TextView tvTitle;
     private ListView ivshopList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
-
+        //隱藏標題欄
         setContentView(R.layout.activity_main);
+
+        View title_1 = findViewById(R.id.title_1);
+        TextView tv1 = (TextView) title_1.findViewById(R.id.tv_title);
+        tv1.setText("首页");
+        ImageView imageView = findViewById(R.id.icon);
+        imageView.setImageResource(R.drawable.menu_48);
+
+        //設置include中標題欄的textview
+
         initView();
     }
-    public void user(View v) {
+    public void onClick(View v) {
         Intent it = new Intent(this, user.class);
-
         startActivity(it);
     }
 
     private void initView() {
         tvTitle=findViewById(R.id.tv_title);
         ivshopList=findViewById(R.id.iv_shop_list);
+
         List<String> list=new ArrayList<>();
-       // for (int i=0;i<20;i++){
-        //    list.add(i+"-"+i);
-        //}
         ArrayAdapter adapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,android.R.id.text1,list.toArray());
         ivshopList.setAdapter(adapter);
     }
--- flashDrop/app/src/main/java/fcu/flashDrop/user.java
+++ flashDrop/app/src/main/java/fcu/flashDrop/user.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class user extends AppCompatActivity {
 
@@ -16,6 +18,15 @@ public class user extends AppCompatActivity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_user);
+        View title_1 = findViewById(R.id.title_1);
+        TextView tv1 = (TextView) title_1.findViewById(R.id.tv_title);
+        tv1.setText("個人主頁");
+        ImageView imageView = findViewById(R.id.icon);
+        imageView.setImageResource(R.drawable.go_back);
+    }
+    public void onClick(View v) {
+        Intent it = new Intent(this, MainActivity.class);
+        startActivity(it);
     }
     public void reg(View v) {
         Intent it = new Intent(this, reg.class);
